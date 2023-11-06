d02d5addba148b8e633ca68c7e3a76adb2d9d59a
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-11-06 17:18:37 -0500

    send email

--- src/server/app/services/email/__init__.py
+++ src/server/app/services/email/__init__.py
@@ -1,5 +1,8 @@
 import os
 from dotenv import load_dotenv
+from email.message import EmailMessage
+import ssl
+import smtplib
 
 # Load the environment variables
 load_dotenv()
@@ -21,5 +24,18 @@ class EmailService:
         """
         
         print("Sending Email")
+        
+        em = EmailMessage()
+        em['From'] = email_sender
+        em['To'] = email_receiver
+        em['Subject'] = subject
+        em.set_content(body)
+        
+        context = ssl.create_default_context()
+        
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(email_sender, email_password)
+            smtp.sendmail(email_sender, email_receiver, em.as_string())
+        
     
     pass
