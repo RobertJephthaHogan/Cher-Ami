0b3d68137549544b309305fa1a88f7d920caccfd
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-11-10 14:32:01 -0500

    Main layout

---
+++ src/client/src/layouts/MainLayout/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './styles.css'
+import SideBar from "./SideBar";
+import LayoutHeader from "./Header";
+import { Outlet } from "react-router-dom";
+
+
+export default function MainLayout() {
+
+    return (
+        <div className="main-layout">
+            <LayoutHeader/>
+
+            <div className="layout-body">
+                <SideBar/>
+                <Outlet/>
+            </div>
+        </div>
+    )
+}
