eb73acccd7a4ce41317cb980d9e472b38958cb22
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-11-10 14:32:16 -0500

    layout sidebar

---
+++ src/client/src/layouts/MainLayout/SideBar/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './styles.css'
+import { useNavigate } from 'react-router'
+
+
+export default function SideBar() {
+
+    const navigate = useNavigate()
+
+    const menuItems = [
+        {
+            title: 'Dashboard',
+            route: '/dashboard'
+        },
+        {
+            title: 'My Workouts',
+            route: '/my-workouts'
+        },
+        {
+            title: 'My Nutrition',
+            route: '/my-nutrition'
+        },
+        {
+            title: 'Workout Plans',
+            route: '/workout-plans'
+        },
+        {
+            title: 'Nutrition Plans',
+            route: '/nutrition-plans'
+        },
+    ]
+
+
+    return (
+        <div className='sidebar'>
+            {
+                menuItems?.map((item: any, i: any) => {
+                    return (
+                        <div 
+                            className='sidebar-row'
+                            onClick={() => navigate(item?.route)}
+                            key={`sidebar-row-${i}`}
+                        >
+                            <span className='sidebar-row-title'>{item?.title}</span>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
