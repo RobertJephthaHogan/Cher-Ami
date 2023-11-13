f951fcd20aadee7876b7c7e8f1d31753e7b97081
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-11-13 15:08:54 -0500

    settings page topbar

--- src/client/src/pages/Settings/index.tsx
+++ src/client/src/pages/Settings/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
-
+import './styles.css'
+import HomeOutlined from '@ant-design/icons/HomeOutlined'
 
 
 export default function Settings() {
 
     return (
-        <div>
+        <div className='settings-page'>
+            <div className='settings-topbar'>
+                <div className='settings-topbar-content'>
+                    <span className='topbar-home-icon-container'>
+                        <HomeOutlined/>
+                    </span>
+                    <span className='topbar-path-container'>
+                        {'>'}
+                    </span>
+                    <span className='topbar-settings-text'>
+                        Settings
+                    </span>
+                </div>
+                <div className='topbar-bottom-divider'/>
+            </div>
             Settings
         </div>
     )
