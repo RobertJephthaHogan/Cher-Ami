06fedfed7076020d9555e1978565ddcb0c13dfe3
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-11-14 20:46:15 -0500

    Contacts page header bar

--- src/client/src/pages/Contacts/index.tsx
+++ src/client/src/pages/Contacts/index.tsx
@@ -7,9 +7,24 @@ export default function Contacts() {
     return (
         <div className='contacts-page'>
             <div className='cp-left-panel'>
+                <div className='cp-lp-header'>
+                    <span className='cp-lp-header-text'>
+                        Filters
+                    </span>
+                </div>
                 left
             </div>
-            <div>
+            <div className='cp-right-panel'>
+                <div className='cp-rp-header'>
+                    <div>
+                        <span className='cp-rp-header-text'>
+                            Contacts
+                        </span>
+                    </div>
+                    <div>
+                        Buttons
+                    </div>
+                </div>
                 right
             </div>
         </div>
