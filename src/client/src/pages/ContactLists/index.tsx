2ce01d065ccc63088482234ac17ce57b6c9b1546
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-11-20 18:39:01 -0500

    ContactLists Topbar UI

--- src/client/src/pages/ContactLists/index.tsx
+++ src/client/src/pages/ContactLists/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
-
+import './styles.css'
+import { Button } from 'antd'
 
 
 export default function ContactLists() {
 
     return (
-        <div>
-            Contact Lists
+        <div className='contact-lists-component'>
+            <div className='cl-topbar'>
+                <div className='cl-topbar-left'>
+                    <span className='cl-title-text'>
+                        Contact Lists
+                    </span>
+                </div>
+                <div className='cl-topbar-right'>
+                    <Button className='cncl-btn'>
+                        Create New Contact List
+                    </Button>
+                    <Button type='primary'>
+                        Add Contact To Existing List
+                    </Button>
+                </div>
+            </div>
+            Contact Lists Content
         </div>
     )
 }
