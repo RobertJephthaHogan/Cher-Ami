42547aa9d5e7323555161e3d3212bd111e7de2ef
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-11-20 12:59:09 -0500

    Contact List rows

--- src/client/src/components/ContactListSelector/index.tsx
+++ src/client/src/components/ContactListSelector/index.tsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { store } from '../../redux/store'
+import contactActions from '../../redux/actions/contact'
+import contactListActions from '../../redux/actions/contactList'
 
 
 
 export default function ContactListSelector() {
 
+    const currentUser = useSelector((state: any) => state.user?.data ?? [])
+    const userContactLists = useSelector((state: any) => state.contactLists?.queryResult ?? [])
+
+    
+    useEffect(() => {
+        setComponentData()
+    }, [])
+
+    function setComponentData() {
+        store.dispatch(contactActions.setContacts(currentUser?._id))
+        store.dispatch(contactListActions.setContactLists(currentUser?._id))
+    }
+
+    const rows = userContactLists?.map((cl:any) => {
+        console.log('cl', cl)
+        return (
+            <div className='contact-list-row'>
+                <span className='cl-name-text'>
+                    {cl?.name}
+                </span>
+            </div>
+        )
+    })
+
     return (
-        <div>
-            Contact List Selector
+        <div className='contact-list-selector'>
+            {rows}
         </div>
     )
 }
