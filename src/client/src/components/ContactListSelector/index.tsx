211b54b73aec2472807062c11c3ae9f941e4c668
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-11-20 13:35:46 -0500

    Selected contact row styling

--- src/client/src/components/ContactListSelector/index.tsx
+++ src/client/src/components/ContactListSelector/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { store } from '../../redux/store'
 import contactActions from '../../redux/actions/contact'
@@ -14,7 +14,7 @@ export default function ContactListSelector(props: ContactListSelectorProps) {
 
     const currentUser = useSelector((state: any) => state.user?.data ?? [])
     const userContactLists = useSelector((state: any) => state.contactLists?.queryResult ?? [])
-
+    const [selectedRowIndex, setSelectedRowIndex] = useState<any>()
     
     useEffect(() => {
         setComponentData()
@@ -25,12 +25,18 @@ export default function ContactListSelector(props: ContactListSelectorProps) {
         store.dispatch(contactListActions.setContactLists(currentUser?._id))
     }
 
-    const rows = userContactLists?.map((cl:any) => {
-        console.log('cl', cl)
+    function onRowSelect(rowData: any, index: any) {
+        props.onSelect(rowData)
+        setSelectedRowIndex(index)
+    }
+
+    const rows = userContactLists?.map((cl:any, i: any) => {
         return (
             <div 
-                className='contact-list-row'
-                onClick={() => props.onSelect(cl)}
+                className={`contact-list-row ${
+                    selectedRowIndex == i && 'selected-row'
+                }`}
+                onClick={() => onRowSelect(cl, i)}
             >
                 <span className='cl-name-text'>
                     {cl?.name}
