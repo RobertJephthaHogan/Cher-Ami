fe15b6aa0997de8495e02feddaa86c7cabaf5148
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-11-20 21:05:31 -0500

    Contact Lists table mock

--- src/client/src/pages/ContactLists/index.tsx
+++ src/client/src/pages/ContactLists/index.tsx
@@ -1,10 +1,94 @@
 import React, { useEffect, useState } from 'react'
 import './styles.css'
-import { Button, Input, Modal } from 'antd'
+import { Button, Input, Modal, Space, Table, Tag  } from 'antd'
+import type { ColumnsType } from 'antd/es/table';
 import { useSelector } from 'react-redux'
 import { store } from '../../redux/store'
 import contactListActions from '../../redux/actions/contactList'
 
+
+
+
+interface DataType {
+    key: string;
+    name: string;
+    age: number;
+    address: string;
+    tags: string[];
+}
+  
+const columns: ColumnsType<DataType> = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: 'Tags',
+      key: 'tags',
+      dataIndex: 'tags',
+      render: (_, { tags }) => (
+        <>
+          {tags.map((tag) => {
+            let color = tag.length > 5 ? 'geekblue' : 'green';
+            if (tag === 'loser') {
+              color = 'volcano';
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (_, record) => (
+        <Space size="middle">
+          <a>Invite {record.name}</a>
+          <a>Delete</a>
+        </Space>
+      ),
+    },
+];
+  
+const data: DataType[] = [
+    {
+        key: '1',
+        name: 'John Brown',
+        age: 32,
+        address: 'New York No. 1 Lake Park',
+        tags: ['nice', 'developer'],
+    },
+    {
+        key: '2',
+        name: 'Jim Green',
+        age: 42,
+        address: 'London No. 1 Lake Park',
+        tags: ['loser'],
+    },
+    {
+        key: '3',
+        name: 'Joe Black',
+        age: 32,
+        address: 'Sydney No. 1 Lake Park',
+        tags: ['cool', 'teacher'],
+    },
+];
 
 export default function ContactLists() {
     
@@ -65,6 +149,12 @@ export default function ContactLists() {
                     placeholder='Search Contact Lists...'
                 />
             </div>
+
+            <div className='table-container'>
+                <Table columns={columns} dataSource={data} />
+            </div>
+
+            
 
             Contact Lists Content
 
