c5b5d3a29a119671f8f38518294ba11c4ce26503
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-11-20 21:10:47 -0500

    Contacts table mock

--- src/client/src/pages/Contacts/index.tsx
+++ src/client/src/pages/Contacts/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './styles.css'
-import { Button, Input, Modal } from 'antd'
+import { Button, Input, Modal, Space, Table, Tag  } from 'antd'
+import type { ColumnsType } from 'antd/es/table';
 import UploadOutlined from '@ant-design/icons/UploadOutlined'
 import ContactForm from '../../components/ContactForm'
 import { useSelector } from 'react-redux'
@@ -8,6 +9,95 @@ import { store } from '../../redux/store'
 import contactActions from '../../redux/actions/contact'
 import ContactListUploader from '../../components/ContastListUploader'
 import contactListActions from '../../redux/actions/contactList'
+
+
+
+
+
+
+
+interface DataType {
+    key: string;
+    name: string;
+    age: number;
+    address: string;
+    tags: string[];
+}
+  
+const columns: ColumnsType<DataType> = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: 'Tags',
+      key: 'tags',
+      dataIndex: 'tags',
+      render: (_, { tags }) => (
+        <>
+          {tags.map((tag) => {
+            let color = tag.length > 5 ? 'geekblue' : 'green';
+            if (tag === 'loser') {
+              color = 'volcano';
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (_, record) => (
+        <Space size="middle">
+          <a>Invite {record.name}</a>
+          <a>Delete</a>
+        </Space>
+      ),
+    },
+];
+  
+const data: DataType[] = [
+    {
+        key: '1',
+        name: 'John Brown',
+        age: 32,
+        address: 'New York No. 1 Lake Park',
+        tags: ['nice', 'developer'],
+    },
+    {
+        key: '2',
+        name: 'Jim Green',
+        age: 42,
+        address: 'London No. 1 Lake Park',
+        tags: ['loser'],
+    },
+    {
+        key: '3',
+        name: 'Joe Black',
+        age: 32,
+        address: 'Sydney No. 1 Lake Park',
+        tags: ['cool', 'teacher'],
+    },
+];
+
+
 
 
 export default function Contacts() {
@@ -79,7 +169,7 @@ export default function Contacts() {
                     />
                 </div>
                 <div className='table-container'>
-                    Table Container
+                    <Table columns={columns} dataSource={data} />
                 </div>
             </div>
             
