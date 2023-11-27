35e8f4ef46305d63d0fc48ec288e88cc6607fb81
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-11-27 18:47:22 -0500

    create emailCampaign.service.js

---
+++ src/client/src/services/emailCampaign.service.js
@@ -0,0 +1,109 @@
+import { getConfig } from '../config/Constants'
+import axios from 'axios'
+
+
+const config = getConfig()
+
+const apiInstance = axios.create({
+    baseURL: `${config.apiUrl}/`,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
+
+
+export const emailCampaignService = {
+    getEmailCampaigns,
+    getUserEmailCampaigns,
+    createEmailCampaign,
+    getAEmailCampaign,
+    updateEmailCampaign, 
+    deleteEmailCampaign
+}
+
+
+async function getEmailCampaigns() {
+    return new Promise((resolve, reject) => {
+        apiInstance
+            .get(`/email_campaign/get_all`)
+            .then((response) => {
+                const resp = response.data
+                return resolve(resp)
+            })
+            .catch((error) => {
+                console.error(error)
+                reject(error)
+            })
+    })
+}
+
+async function getUserEmailCampaigns(userID) {
+    return new Promise((resolve, reject) => {
+        apiInstance
+            .get(`/email_campaign/user/${userID}`)
+            .then((response) => {
+                return resolve(response)
+            })
+            .catch((error) => {
+                console.error(error)
+                reject(error)
+            })
+    })
+}
+
+async function createEmailCampaign(emailCampaignData) {
+    return new Promise((resolve, reject) => {
+        apiInstance
+            .post(`/email_campaign/new`, emailCampaignData)
+            .then((response) => {
+                return resolve(response)
+            })
+            .catch((error) => {
+                console.error(error)
+                reject(error)
+            })
+    })
+}
+
+async function getAEmailCampaign(emailCampaignID) {
+    return new Promise((resolve, reject) => {
+        apiInstance
+            .get(`/email_campaign/${emailCampaignID}`)
+            .then((response) => {
+                return resolve(response)
+            })
+            .catch((error) => {
+                console.error(error)
+                reject(error)
+            })
+    })
+}
+
+async function updateEmailCampaign(emailCampaignID, newEmailCampaign) {
+    return new Promise((resolve, reject) => {
+        apiInstance
+            .put(`/email_campaign/${emailCampaignID}`, newEmailCampaign)
+            .then((response) => {
+                return resolve(response)
+            })
+            .catch((error) => {
+                console.error(error)
+                reject(error)
+            })
+    })
+}
+
+async function deleteEmailCampaign(emailCampaignID) {
+    return new Promise((resolve, reject) => {
+        apiInstance
+            .delete(`/email_campaign/${emailCampaignID}`)
+            .then((response) => {
+                return resolve(response)
+            })
+            .catch((error) => {
+                console.error(error)
+                reject(error)
+            })
+    })
+}
+
