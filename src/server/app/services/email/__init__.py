3eb01104d508080dd58b87af386d1252bdc803ae
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-12-02 15:34:14 -0500

    one-time email submission latest

--- src/server/app/services/email/__init__.py
+++ src/server/app/services/email/__init__.py
@@ -14,7 +14,40 @@ class EmailService:
     def __init__(self, **kwargs):
         self.kwargs = kwargs
 
-    
+            
+            
+    def testSendEmail(self):
+                
+        email_sender = 'contact.rjh.ventures@gmail.com'
+        email_password = os.getenv("GMAIL_TFA_PASSWORD")
+        email_receiver = 'robertjephthahogan@gmail.com'
+        
+        subject = 'This is the email subject'
+        
+        body = """
+        This is the body of the email content. It worked!
+        """
+        
+        print("Sending Email")
+        
+        em = EmailMessage()
+        em['From'] = email_sender
+        em['To'] = email_receiver
+        em['Subject'] = subject
+        em.set_content(body)
+        
+        context = ssl.create_default_context()
+        
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            try:
+                smtp.login(email_sender, email_password)
+                resp = smtp.sendmail(email_sender, email_receiver, em.as_string())
+                return resp
+            except Exception as e:
+                print('error', e)
+                return {'error': e}
+            
+            
     def sendEmail(self):
                 
         email_sender = 'contact.rjh.ventures@gmail.com'
--- src/server/app/services/email_campaign/__init__.py
+++ src/server/app/services/email_campaign/__init__.py
@@ -12,13 +12,26 @@ class EmailCampaignService:
     
     async def create_one_time_email_campaign(campaign_data):
         
-        # Need to create the emailCampaign no matter what
-        # after email campaign is created, start checking if send initial is true
-        # send the one time email campaign, then update the status of the email campaign in the db
-        
-        print('campaign_data', campaign_data)
-        
+        # Create the emailCampaign entry in db no matter what, then change status accordingly
         new_email_campaign = await EmailCampaignOperations.add_email_campaign(campaign_data)
+        
+        # After email campaign is created, Check if send initial is true
+        shouldSendInitial = campaign_data.frequency.get('sendOtInitial')
+        
+        if shouldSendInitial:
+            print('Should Send Initial')
+            # send the one time email campaign immediately
+            # then update the status data of the email campaign in the db
+            
+        
+        if not shouldSendInitial:
+            print('Schedule the email to be sent')
+            print('by adding an entry to the schedules services table')
+            
+            # Schedule the one time email campaign
+            # then update the status data of the email campaign in the db
+        
+        
         
         return new_email_campaign
     
