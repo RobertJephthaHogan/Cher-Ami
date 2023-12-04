91e242caa9609663bd9babec9cac73fe5550c81b
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-12-03 22:25:38 -0500

    update ScheduledService model

--- src/server/app/models/ScheduledService.py
+++ src/server/app/models/ScheduledService.py
@@ -9,6 +9,7 @@ class ScheduledService(Document):
     action: Optional[str] = Field(...)
     createdByUserId: str = Field(...)
     target_id: str = Field(...)
+    executed: bool = Field(...)
     time: datetime.datetime = Field(...)
     
     
@@ -22,6 +23,7 @@ class ScheduledService(Document):
                 "action": 'send-scheduled-one-time-email',
                 "createdByUserId": "wwv45yw4gw45w76nr657eu",
                 "target_id": "123",
+                "executed": False,
                 "time": "2022-12-22T16:09:23.443Z",
             }
         }
@@ -32,6 +34,7 @@ class UpdateScheduledServiceModel(BaseModel):
     action: Optional[str] 
     createdByUserId: Optional[str]
     target_id: Optional[str]
+    executed: Optional[bool]
     time: Optional[datetime.datetime]
 
     class Config:
@@ -41,6 +44,7 @@ class UpdateScheduledServiceModel(BaseModel):
                 "action": 'send-scheduled-one-time-email',
                 "createdByUserId": "wwv45yw4gw45w76nr657eu",
                 "target_id": "123",
+                "executed": False,
                 "time": "2022-12-22T16:09:23.443Z",
             }
         }
