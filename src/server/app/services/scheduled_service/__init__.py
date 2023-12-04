42b54073d922b04a794b3397df35eb73d9a31647
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-12-03 22:16:48 -0500

    async scheduler

--- src/server/app/services/scheduled_service/__init__.py
+++ src/server/app/services/scheduled_service/__init__.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, BackgroundTasks
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.asyncio  import AsyncIOScheduler
 from datetime import datetime
-import time
+from app.database.scheduled_service_operations import ScheduledServiceOperations
 
 
 # Mock data
-scheduled_services = [
+mocked_scheduled_services = [
     {"id": 1, "date_time": "2023-12-02 10:30:00", "action": "send_email"},
 ]
 
@@ -13,7 +13,7 @@ scheduled_services = [
 class ScheduledServiceService: # as agonizing as this class name is, I'll continue to follow the convention I have been 
     
     def startScheduler(self):
-        scheduler = BackgroundScheduler()
+        scheduler = AsyncIOScheduler()
         scheduler.add_job(self.check_scheduled_tasks, "interval", seconds=10)  # Check every 10 seconds
         scheduler.start()
         
@@ -21,14 +21,20 @@ class ScheduledServiceService: # as agonizing as this class name is, I'll contin
         # Implement your scheduled task logic here
         print(f"Performing task for service {service_id}: {action} at {datetime.now()}")
 
-    def check_scheduled_tasks(self):
+    async def check_scheduled_tasks(self):
+        
         current_time = datetime.now()
-        for service in scheduled_services:
+        
+        scheduled_services = await ScheduledServiceOperations.retrieve_all_scheduled_services()
+        
+        print('scheduled_services', scheduled_services)
+        
+        for service in mocked_scheduled_services:
             task_time = datetime.strptime(service["date_time"], "%Y-%m-%d %H:%M:%S")
             if current_time >= task_time:
                 self.perform_scheduled_task(service["id"], service["action"])
     
     def shutdownScheduler(self):
-        scheduler = BackgroundScheduler()
+        scheduler = AsyncIOScheduler()
         scheduler.shutdown()
         
