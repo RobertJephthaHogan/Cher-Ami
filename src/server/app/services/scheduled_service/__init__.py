6b757c930d8880fb9ca580015305844a2a86f0f2
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-12-04 13:25:18 -0500

    perform scheduled service handling

--- src/server/app/services/scheduled_service/__init__.py
+++ src/server/app/services/scheduled_service/__init__.py
@@ -17,22 +17,45 @@ class ScheduledServiceService: # as agonizing as this class name is, I'll contin
         scheduler.add_job(self.check_scheduled_tasks, "interval", seconds=10)  # Check every 10 seconds
         scheduler.start()
         
-    def perform_scheduled_task(self, service_id: int, action: str):
-        # Implement your scheduled task logic here
+    def perform_scheduled_task(self, service_id: int, action: str, target_it: str):
+        
         print(f"Performing task for service {service_id}: {action} at {datetime.now()}")
+        
+        if action == 'send-scheduled-one-time-email-campaign':
+            # TODO: Send scheduled one time email campaign
+            # get email campaign data from db using target_id
+            # send the campaign
+            
+            pass    
+        
+        elif action == 'send-recurring-email-campaign':
+            # TODO: Send recurring email campaign instance, 
+            # Set executed to true for the executed scheduled service
+            # schedule the next instance of the campaign if before the campaign end date
+            pass
+
+        else:
+            return 'This should never fire'
+        
 
     async def check_scheduled_tasks(self):
         
         current_time = datetime.now()
         
-        scheduled_services = await ScheduledServiceOperations.retrieve_all_scheduled_services()
+        scheduled_services = await ScheduledServiceOperations.retrieve_unexecuted_scheduled_services()
         
         print('scheduled_services', scheduled_services)
         
-        for service in mocked_scheduled_services:
-            task_time = datetime.strptime(service["date_time"], "%Y-%m-%d %H:%M:%S")
+        for service in scheduled_services:
+            task_time = service.time
             if current_time >= task_time:
-                self.perform_scheduled_task(service["id"], service["action"])
+                self.perform_scheduled_task(service["id"], service["action"], service["target_id"])
+            
+        
+        # for service in mocked_scheduled_services:
+        #     task_time = datetime.strptime(service["date_time"], "%Y-%m-%d %H:%M:%S")
+        #     if current_time >= task_time:
+        #         self.perform_scheduled_task(service["id"], service["action"])
     
     def shutdownScheduler(self):
         scheduler = AsyncIOScheduler()
