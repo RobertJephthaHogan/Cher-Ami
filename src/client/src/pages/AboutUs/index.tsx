cff66dc1b7240be01f2e8fc4ef4534e8b83e8345
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-12-10 18:51:27 -0500

    init about us page

---
+++ src/client/src/pages/AboutUs/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HomepageHeader from '../../components/HomepageHeader'
+
+
+
+export default function AboutUs() {
+
+    return (
+        <div className='about-us'>
+            <HomepageHeader/>
+            About Us
+        </div>
+    )
+}
