92dba317375be579c6a3d92ea0877d66b229374c
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-12-11 13:04:51 -0500

    update user signin dto

--- src/client/src/pages/SignUp/index.tsx
+++ src/client/src/pages/SignUp/index.tsx
@@ -30,6 +30,7 @@ export default function SignUp() {
             receiveToPhone: '',
             sendFromEmailAddresses: [],
             sendFromPhoneNumbers: [],
+            twilioCredentials: {},
             password: data?.password,
             role: 'user',
         }
