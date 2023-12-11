cfd87dc7797778142f336239dea48b2f25e4a459
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-12-10 19:14:40 -0500

    about us banner

--- src/client/src/pages/AboutUs/index.tsx
+++ src/client/src/pages/AboutUs/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import HomepageHeader from '../../components/HomepageHeader'
-
+import './styles.css'
 
 
 export default function AboutUs() {
@@ -8,7 +8,10 @@ export default function AboutUs() {
     return (
         <div className='about-us'>
             <HomepageHeader/>
-            About Us
+            <div className='about-us-banner'/>
+            <div className='about-us-body'>
+                About Us
+            </div>
         </div>
     )
 }
