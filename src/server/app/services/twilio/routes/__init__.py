80d707be40855ce79fd08cc43179e8d92e8e4fe9
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-12-10 22:28:32 -0500

    get all twilio subaccounts

--- src/server/app/services/twilio/routes/__init__.py
+++ src/server/app/services/twilio/routes/__init__.py
@@ -16,24 +16,40 @@ class TwilioRouter:
     @router.get("/test", response_description="Test Twilio", response_model=Response)
     async def testEmail():
         
-        print('Testing TWILIO')
-        
-        # Find your Account SID and Auth Token at twilio.com/console
-        # and set the environment variables. See http://twil.io/secure
         account_sid = os.environ['TWILIO_ACCOUNT_SID']
         auth_token = os.environ['TWILIO_AUTH_TOKEN']
         client = Client(account_sid, auth_token)
-
         accounts = client.api.v2010.accounts.list(limit=20)
-        
         sub_accounts = []
 
         for record in accounts:
-            print(record.sid)
             account = client.api.accounts(record.sid).fetch()
-            print('account', account.__dict__)
             sub_accounts.append(account.__dict__)
+            
+        return {
+                "status_code": 200,
+                "response_type": "success",
+                "description": "Email sent successfully",
+                "data": sub_accounts
+            }
+        
+        
+    @router.get("/get_all_sub_accounts", response_description="Getting All Twilio SubAccounts", response_model=Response)
+    async def get_all_sub_accounts():
+        
+        # TODO: Integrate Twilio user account information into system User model
+        # Then proceed to build this service out taking in the users id
+        # and finding the matching twilio credentials in the db 
+        
+        account_sid = os.environ['TWILIO_ACCOUNT_SID']
+        auth_token = os.environ['TWILIO_AUTH_TOKEN']
+        client = Client(account_sid, auth_token)
+        accounts = client.api.v2010.accounts.list(limit=20)
+        sub_accounts = []
 
+        for record in accounts:
+            account = client.api.accounts(record.sid).fetch()
+            sub_accounts.append(account.__dict__)
             
         return {
                 "status_code": 200,
