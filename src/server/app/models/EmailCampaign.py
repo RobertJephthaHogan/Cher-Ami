684a675aa43cbccca8fb5b6f5d81e7b1a0a56a9d
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-12-12 15:19:57 -0500

    update EmailCampaign model

--- src/server/app/models/EmailCampaign.py
+++ src/server/app/models/EmailCampaign.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel, EmailStr, Field
 class EmailCampaign(Document):
     id: Optional[str] = Field(...)
     title: str = Field(...)
+    emailSubject: str = Field(...)
     sendFromEmail: EmailStr = Field(...)
     emailBody: str = Field(...)
     recipientContactLists: List[Any] = Field(...)
@@ -24,6 +25,7 @@ class EmailCampaign(Document):
             "example": {
                 "id": "6382e2abc07256ef099af572",
                 "title": "Marketing Email",
+                "emailSubject": "Email Subject",
                 "sendFromEmail": "myBusinessEmail@gmail.com",
                 "emailBody": "This is a basic email body",
                 "recipientContactLists": [],
@@ -40,6 +42,7 @@ class EmailCampaign(Document):
 class UpdateEmailCampaignModel(BaseModel):
     id: Optional[str]
     title: Optional[str]
+    emailSubject: Optional[str]
     sendFromEmail: Optional[EmailStr]
     emailBody: Optional[str]
     recipientContactLists: Optional[List[Any]]
@@ -53,6 +56,7 @@ class UpdateEmailCampaignModel(BaseModel):
             "example": {
                 "id": "6382e2abc07256ef099af572",
                 "title": "Marketing Email",
+                "emailSubject": "Email Subject",
                 "sendFromEmail": "myBusinessEmail@gmail.com",
                 "emailBody": "This is a basic email body",
                 "recipientContactLists": [],
