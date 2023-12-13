4bd690b5b2bc8b0ad7bd79f90cd06e8ecbd5f893
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-12-13 15:54:49 -0500

    Coming soon component

--- src/client/src/pages/PhoneCall/index.tsx
+++ src/client/src/pages/PhoneCall/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
-
+import ComingSoon from '../../components/ComingSoon'
+import './styles.css'
 
 
 export default function PhoneCall() {
 
     return (
-        <div>
-            Phone Call Component
+        <div className='phone-call-component'>
+            <ComingSoon/>
         </div>
     )
 }
