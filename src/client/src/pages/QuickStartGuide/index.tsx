a41a69c1c80958c88fa7cde8a4a1d0aee9632635
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2023-12-15 16:31:01 -0500

    Quick Start Guide coming soon

--- src/client/src/pages/QuickStartGuide/index.tsx
+++ src/client/src/pages/QuickStartGuide/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HomepageHeader from '../../components/HomepageHeader'
+import ComingSoon from '../../components/ComingSoon'
 
 
 
@@ -8,7 +9,13 @@ export default function QuickStartGuide() {
     return (
         <div>
             <HomepageHeader/>
-            Quick Start Guide
+            <div
+                style={{
+                    marginTop: '100px'
+                }}
+            >
+                <ComingSoon/>
+            </div>
         </div>
     )
 }
