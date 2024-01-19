2060ced28d020f48513dc513134af34312c996f1
Robert Hogan <81276248+RobertJephthaHogan@users.noreply.github.com>
2024-01-19 16:41:39 -0500

    Our Platform section

--- src/client/src/pages/AboutUs/index.tsx
+++ src/client/src/pages/AboutUs/index.tsx
@@ -7,48 +7,12 @@ import './styles.css'
 export default function AboutUs() {
 
 
-    const timelineItems = [
-        {
-            title: "May 1940",
-            cardTitle: "Dunkirk",
-            //url: "http://www.history.com",
-            cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-            cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        }, 
-        {
-            title: "May 1940",
-            cardTitle: "Dunkirk",
-            //url: "http://www.history.com",
-            cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-            cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        }, 
-        {
-            title: "May 1940",
-            cardTitle: "Dunkirk",
-            url: "http://www.history.com",
-            cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-            cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        }, 
-        {
-            title: "May 1940",
-            cardTitle: "Dunkirk",
-            url: "http://www.history.com",
-            cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-            cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        }, 
-        {
-            title: "May 1940",
-            cardTitle: "Dunkirk",
-            url: "http://www.history.com",
-            cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-            cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-        }, 
-    ];
-
     return (
         <div className='about-us'>
             <HomepageHeader/>
+
             <div className='about-us-banner'/>
+
             <div className='about-us-body'>
                 <div className='aub-info-wrapper'>
                     <div className='aub-info'>
@@ -61,12 +25,8 @@ export default function AboutUs() {
                             <span className='about-us-info'>
                                 We’re building an omnichannel communication 
                                 platform to help businesses of all sizes level up their 
-                                customer engagement.
-                            </span>
-                        </div>
-                        <div className='aub-info-content'>
-                            <span className='about-us-info'>
-                                At Cher-Ami Technologies, our mission is to provide a cutting
+                                customer engagement. At Cher-Ami Technologies, 
+                                our mission is to provide a cutting
                                 edge omnichannel communication platform to help businesses
                                 of all sizes level up their customer engagement. We believe
                                 companies of all sizes should be able to provide high quality
@@ -90,34 +50,134 @@ export default function AboutUs() {
                     </div>
                 </div>
             </div>
-            <div className='about-us-body'>
-                <div className='aub-info-wrapper'>
-                    <div className='aub-info'>
-                        <div  className='about-us-title-wrapper'>
-                            <span className='about-us-title'>
-                                Timeline
+
+            <div className='platform-section'>
+                <div className='ps-info-wrapper'>
+                    <div className='ps-info'>
+                        <div className='platform-section-title-wrapper'>
+                            <span className='platform-section-title'>
+                                Our Platform
                             </span>
                         </div>
                     </div>
+                    <div className='platform-section-rows'>
+
+                        <div className='ps-row'>
+                            <div className='ps-row-left'>
+                                <div className='oci-info-container-l'>
+                                    <span className='oci-pre-info'>
+                                        Omni-Channel Inbox
+                                    </span>
+                                    <span className='oci-title-info'>
+                                        One Inbox For All Channels
+                                    </span>
+                                    <span className='oci-sub-info'>
+                                        Bring your customer conversations together. 
+                                        Empower customer service agents to support 
+                                        customers more efficiently, without compromising on 
+                                        customer experience. 
+                                    </span>
+                                </div>   
+                            </div>
+                            <div className='ps-row-right'>
+                                <div 
+                                    className='oci-image-container'
+                                    //temporary styles below (remove when adding img)
+                                    style={{
+                                        display: "flex",
+                                        justifyContent: "center",
+                                        alignItems: "center",
+                                        textAlign: 'center'
+                                        
+                                    }}
+                                >
+                                    Image Design <br/> 
+                                    in progress!
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='ps-row'>
+                            <div className='ps-row-left'>
+                                <div 
+                                    className='oci-image-container'
+                                    //temporary styles below (remove when adding img)
+                                    style={{
+                                        display: "flex",
+                                        justifyContent: "center",
+                                        alignItems: "center",
+                                        textAlign: 'center'
+                                        
+                                    }}
+                                >
+                                    Image Design <br/> 
+                                    in progress!
+                                </div>
+                            </div>
+                            <div className='ps-row-right'>
+                                <div className='oci-info-container-r'>
+                                    <span className='oci-pre-info'>
+                                        Contacts Managed Easily
+                                    </span>
+                                    <span className='oci-title-info'>
+                                        Easily Manage Contacts 
+                                        and Contact Lists
+                                    </span>
+                                    <span className='oci-sub-info'>
+                                        Easily dispatch emails, texts, and voice campaigns to 
+                                        one or many of your predefined contact lists. or use 
+                                        our contact dashboard to filter through your contacts 
+                                        and generate new contact lists.
+                                    </span>
+                                </div>   
+                                
+                            </div>
+                        </div>
+
+                        <div className='ps-row'>
+                            <div className='ps-row-left'>
+                                <div className='oci-info-container-l'>
+                                    <span className='oci-pre-info'>
+                                        Intuitive Campaign Creation
+                                    </span>
+                                    <span className='oci-title-info'>
+                                        Create Email, Text, and Voice 
+                                        Campaigns Intuitively
+                                    </span>
+                                    <span className='oci-sub-info'>
+                                        Easily create one-time or recurring email, text, and 
+                                        voice campaigns to engage with your customers 
+                                        easily and seamlessly 
+                                    </span>
+                                </div>   
+                            </div>
+                            <div className='ps-row-right'>
+                                <div 
+                                    className='oci-image-container'
+                                    //temporary styles below (remove when adding img)
+                                    style={{
+                                        display: "flex",
+                                        justifyContent: "center",
+                                        alignItems: "center",
+                                        textAlign: 'center'
+                                        
+                                    }}
+                                >
+                                    Image Design <br/> 
+                                    in progress!
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
-            <div className='about-us-timeline-section'>
-                <div style={{ width: '800px', height: '950px' }}>
-                    <Chrono 
-                        items={timelineItems} 
-                        mode="VERTICAL_ALTERNATING" 
-                        itemWidth={800}
-                        theme={{
-                            //primary: 'red',
-                            //secondary: 'blue',
-                            //cardBgColor: 'yellow',
-                            //titleColor: 'black',
-                            //titleColorActive: 'red',
-                        }}
-                    >
-                        
-                    </Chrono>
-                </div>
+            <div
+                style={{
+                    height: '1000px'
+                }}
+            >
+
             </div>
         </div>
     )
